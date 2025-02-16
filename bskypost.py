9f58dd353b37001f28b2c697607cedd3e4848c70
Jaroslav Mikulík <byczech@malyjarda.cz>
2025-02-16 04:46:34 +0100

    Added sending post to Bluesky social network

--- bskypost.py
+++ bskypost.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
-
 import argparse
+import requests
+
+try:
+    import ujson as json
+except (ImportError, SyntaxError):
+    try:
+        import simplejson as json
+    except (ImportError, SyntaxError):
+        import json
+
 from argparse import HelpFormatter
 from functools import partial
+
+from datetime import datetime, timezone
 
 
 class CustomHelpFormatter(HelpFormatter):
@@ -30,7 +41,7 @@ class CustomHelpFormatter(HelpFormatter):
             return ', '.join(parts)
 
 
-""" 
+"""
 With the custom formatter the metavar does not get displayed twice.
 With the max_help_position you can decide how long the parameters + metavar should be before a line break gets inserted,
 additionally the width parameter defines the maximum length of a line.
@@ -40,18 +51,41 @@ https://github.com/alex1701c/Screenshots/blob/master/PythonArgparseCLI/customize
 """
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Describe your project', formatter_class=CustomHelpFormatter)
-    parser.add_argument('-v', '--version', action='version', version='bskypost 1.0')
-    parser.add_argument('-n', '--name', metavar='<name>', help='An optional parameter')
-    parser.add_argument('-e', '--extra', action='store_true', help='This Value is False by default')
+    parser = argparse.ArgumentParser(
+        description='Send post to Bluesky social network', formatter_class=CustomHelpFormatter)
+    parser.add_argument('-v', '--version', action='version',
+                        version='bskypost 1.0')
+    parser.add_argument('bsky_handle', metavar='<bsky_handle>',
+                        help='Bluesky handle')
+    parser.add_argument('app_password', metavar='<app_password>',
+                        help='Bluesky app password')
+    parser.add_argument('post_text', metavar='<post_text>', help='Post text')
     args = parser.parse_args()
 
-    if args.name and args.extra:
-        print('Hello There ' + args.name + '!')
-    elif args.extra:
-        print('Hello There!')
-    elif args.name:
-        print('Hello ' + args.name + '!')
-    else:
-        print('Hello World!')
+    now = datetime.now(timezone.utc).isoformat().replace("+00:00", "Z")
 
+    resp = requests.post(
+        "https://bsky.social/xrpc/com.atproto.server.createSession",
+        json={"identifier": args.bsky_handle, "password": args.app_password},
+    )
+    resp.raise_for_status()
+    session = resp.json()
+    # print(session["accessJwt"])
+
+    post = {
+        "$type": "app.bsky.feed.post",
+        "text": args.post_text,
+        "createdAt": now,
+    }
+
+    resp = requests.post(
+        "https://bsky.social/xrpc/com.atproto.repo.createRecord",
+        headers={"Authorization": "Bearer " + session["accessJwt"]},
+        json={
+            "repo": session["did"],
+            "collection": "app.bsky.feed.post",
+            "record": post,
+        },
+    )
+    print(json.dumps(resp.json(), indent=2))
+    resp.raise_for_status()
